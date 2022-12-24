bd831d16171ef779896f91d116e465de0db971f0
nazmigorkem <nazmigorkem@hotmail.com>
2022-12-25 00:26:37 +0300

    added board and displaying

---
+++ src/board.rs
@@ -0,0 +1,39 @@
+use std::collections::HashSet;
+
+pub struct Board {
+    pegs: HashSet<(u8, u8, bool)>,
+}
+
+impl Board {
+    pub fn new() -> Self {
+        Board {
+            pegs: HashSet::new(),
+        }
+    }
+
+    pub fn play(&mut self, turn: bool, selected_column: u8) {
+        let pegs_count_for_column = self
+            .pegs
+            .iter()
+            .filter(|x| x.1 == selected_column)
+            .collect::<Vec<&(u8, u8, bool)>>()
+            .len() as u8;
+        self.pegs
+            .insert((pegs_count_for_column, selected_column, turn));
+    }
+
+    pub fn print_board(&self) {
+        let mut display_board: Vec<Vec<&str>> = vec![vec!["  "; 7]; 8];
+        for (i, j, player) in self.pegs.iter() {
+            display_board[*i as usize][*j as usize] = if *player { "x " } else { "o " }
+        }
+        display_board.reverse();
+        for i in display_board {
+            for j in i {
+                print!("{j} ");
+            }
+            print!("\n");
+        }
+        print!("\x1b[9F")
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+use board::Board;
+
+pub mod board;
+
+fn main() {
+    let mut board = Board::new();
+    let mut turn = true;
+    let mut choice: u8 = 0;
+    loop {
+        let mut resulting_flag: u8 = 0;
+        let mut buffer = String::new();
+        if let Ok(_) = io::stdin().read_line(&mut buffer) {
+            if let Ok(parsed) = buffer.trim().parse::<u8>() {
+                choice = parsed - 1;
+                if choice > 7 {
+                    resulting_flag = 1;
+                }
+            } else {
+                resulting_flag = 1;
+            }
+        } else {
+            resulting_flag = 2;
+        }
+
+        match resulting_flag {
+            0 => {
+                board.play(turn, choice);
+                board.print_board();
+                turn = !turn;
+            }
+            1 => {
+                println!("Please give an integer between 1 and 7.");
+                continue;
+            }
+            2 => {
+                println!("Error occured while reading the input.");
+                return;
+            }
+            _ => (),
+        }
+    }
+}

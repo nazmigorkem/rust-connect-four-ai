a3caf4c4e378e6398d6207e070368ca864d3e88b
nazmigorkem <nazmigorkem@hotmail.com>
2022-12-25 17:53:17 +0300

    changed printing

--- src/board.rs
+++ src/board.rs
@@ -11,7 +11,7 @@ impl Board {
         }
     }
 
-    pub fn play(&mut self, turn: bool, selected_column: u8) {
+    pub fn play(&mut self, turn: bool, selected_column: u8) -> (u8, String) {
         let pegs_count_for_column = self
             .pegs
             .iter()
@@ -19,14 +19,36 @@ impl Board {
             .collect::<Vec<&(u8, u8, bool)>>()
             .len() as u8;
         if pegs_count_for_column == 8 {
-            println!("You cannot put anymore pegs in this column.");
-            return;
+            return (
+                3,
+                format!(
+                    "You cannot put any more peg to column {}.",
+                    selected_column + 1
+                ),
+            );
         }
         self.pegs
             .insert((pegs_count_for_column, selected_column, turn));
+        return (
+            0,
+            format!(
+                "Player {}, put peg in column {}.",
+                if turn { 1 } else { 2 },
+                selected_column + 1
+            ),
+        );
     }
 
-    pub fn print_board(&self) {
+    pub fn is_game_finished(&self, i: u8, j: u8) -> bool {
+        true
+    }
+
+    pub fn print_board(&self, result: (u8, String)) {
+        let width = 20;
+        println!(
+            "{:=<width$}\n\x1b[2KLast move result: {}\n{:=<width$}",
+            "=", result.1, "=",
+        );
         let mut display_board: Vec<Vec<&str>> = vec![vec!["- "; 7]; 8];
         for (i, j, player) in self.pegs.iter() {
             display_board[*i as usize][*j as usize] = if *player { "x " } else { "o " }
@@ -38,6 +60,6 @@ impl Board {
             }
             print!("\n");
         }
-        print!("\x1b[9F")
+        print!("\x1b[12F");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, thread, time::Duration};
+use std::io;
 
 use board::Board;
 
@@ -8,36 +8,32 @@ fn main() {
     let mut board = Board::new();
     let mut turn = true;
     let mut choice: u8 = 0;
+    println!("PLAYER 1 = x");
+    println!("PLAYER 2 = o");
+    board.print_board((0, String::from("Game started.")));
     loop {
-        let mut resulting_flag: u8 = 0;
+        let mut resulting_flag: (u8, String) = (0, String::new());
         let mut buffer = String::new();
         if let Ok(_) = io::stdin().read_line(&mut buffer) {
             if let Ok(parsed) = buffer.trim().parse::<u8>() {
                 choice = parsed - 1;
                 if choice > 7 {
-                    resulting_flag = 1;
+                    resulting_flag = (1, String::from("Please give an integer between 1 and 7."));
                 }
             } else {
-                resulting_flag = 1;
+                resulting_flag = (1, String::from("Please give an integer between 1 and 7."));
             }
         } else {
-            resulting_flag = 2;
+            resulting_flag = (2, String::from("An error occured while taking input."));
         }
-        match resulting_flag {
-            0 => {
-                board.play(turn, choice);
-                board.print_board();
+
+        if resulting_flag.0 == 0 {
+            resulting_flag = board.play(turn, choice);
+            if resulting_flag.0 == 0 {
                 turn = !turn;
             }
-            1 => {
-                println!("Please give an integer between 1 and 7.");
-                continue;
-            }
-            2 => {
-                println!("Error occured while reading the input.");
-                return;
-            }
-            _ => (),
         }
+
+        board.print_board(resulting_flag);
     }
 }

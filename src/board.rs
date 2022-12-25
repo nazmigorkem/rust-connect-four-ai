897d41f0de10a0e99192d9aa885541dca677b3ee
nazmigorkem <nazmigorkem@hotmail.com>
2022-12-25 18:51:45 +0300

    added goal state check

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 pub struct Board {
-    pegs: HashSet<(u8, u8, bool)>,
+    pub pegs: HashSet<(u8, u8, bool)>,
 }
 
 impl Board {
@@ -11,14 +11,8 @@ impl Board {
         }
     }
 
-    pub fn play(&mut self, turn: bool, selected_column: u8) -> (u8, String) {
-        let pegs_count_for_column = self
-            .pegs
-            .iter()
-            .filter(|x| x.1 == selected_column)
-            .collect::<Vec<&(u8, u8, bool)>>()
-            .len() as u8;
-        if pegs_count_for_column == 8 {
+    pub fn play(&mut self, turn: bool, selected_row: u8, selected_column: u8) -> (u8, String) {
+        if selected_row == 8 {
             return (
                 3,
                 format!(
@@ -27,23 +21,38 @@ impl Board {
                 ),
             );
         }
-        self.pegs
-            .insert((pegs_count_for_column, selected_column, turn));
+        self.pegs.insert((selected_row, selected_column, turn));
         return (
             0,
             format!(
-                "Player {}, put peg in column {}.",
+                "Player {} put peg in column {}.",
                 if turn { 1 } else { 2 },
                 selected_column + 1
             ),
         );
     }
 
-    pub fn is_game_finished(&self, i: u8, j: u8) -> bool {
-        true
+    pub fn is_game_finished(&self, i: u8, j: u8, turn: bool) -> bool {
+        let directions = [(-1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1)];
+        for direction in directions {
+            let mut current_check = direction.clone();
+            let mut count = 1;
+            while let Some(_) =
+                self.pegs
+                    .get(&(i + current_check.0 as u8, j + current_check.1 as u8, turn))
+            {
+                current_check.0 += direction.0;
+                current_check.1 += direction.1;
+                count += 1;
+                if count % 4 == 0 {
+                    return true;
+                }
+            }
+        }
+        false
     }
 
-    pub fn print_board(&self, result: (u8, String)) {
+    pub fn print_board(&self, result: &(u8, String)) {
         let width = 20;
         println!(
             "{:=<width$}\n\x1b[2KLast move result: {}\n{:=<width$}",
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let mut choice: u8 = 0;
     println!("PLAYER 1 = x");
     println!("PLAYER 2 = o");
-    board.print_board((0, String::from("Game started.")));
+    board.print_board(&(0, String::from("Game started.")));
     loop {
         let mut resulting_flag: (u8, String) = (0, String::new());
         let mut buffer = String::new();
@@ -26,14 +26,27 @@ fn main() {
         } else {
             resulting_flag = (2, String::from("An error occured while taking input."));
         }
-
+        let pegs_count_for_column = board
+            .pegs
+            .iter()
+            .filter(|x| x.1 == choice)
+            .collect::<Vec<&(u8, u8, bool)>>()
+            .len() as u8;
         if resulting_flag.0 == 0 {
-            resulting_flag = board.play(turn, choice);
-            if resulting_flag.0 == 0 {
-                turn = !turn;
-            }
+            resulting_flag = board.play(turn, pegs_count_for_column, choice);
         }
 
-        board.print_board(resulting_flag);
+        board.print_board(&resulting_flag);
+        if board.is_game_finished(pegs_count_for_column, choice, turn) {
+            println!(
+                "\x1b[12B\x1b[2KPlayer {} won the game.",
+                if turn { "1" } else { "2" }
+            );
+            break;
+        }
+
+        if resulting_flag.0 == 0 {
+            turn = !turn;
+        }
     }
 }
